2aa0f40173504306feb6c7500859e2073f62daae
azperin <azperin@gmail.com>
2024-06-22 17:40:19 +0500

    basic functional

--- task_2/script.js
+++ task_2/script.js
@@ -1,1 +1,36 @@
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+	const todoInput = document.getElementById('todo-input');
+	const todoList = document.getElementById('todo-list');
+	
+    document.getElementById('todo-form').addEventListener('submit', (e) => {
+        e.preventDefault();
+        const taskText = todoInput.value.trim();
+        if (taskText === '') return;
+        addTodoItem(taskText);
+        todoInput.value = '';
+    });
+
+    function addTodoItem(taskText) {
+        const li = document.createElement('li');
+        const span = document.createElement('span');
+		
+        span.textContent = taskText;
+        span.addEventListener('click', () => {
+            span.parentElement.classList.toggle('completed');
+        });
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', () => {
+            li.remove();
+        });
+
+        li.appendChild(span);
+        li.appendChild(deleteButton);
+        todoList.appendChild(li);
+    };
+});
+
+
+
+
